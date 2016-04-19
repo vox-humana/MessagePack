25d75f6afd5e2fb45336e7349d94d2e0d442fc79
antonmes <antonmes@users.noreply.github.com>
2016-04-19 20:43:48 +0300

    Remove redundant data copying from tests

--- Tests/ExampleTests.swift
+++ Tests/ExampleTests.swift
@@ -2,14 +2,6 @@
 @testable import C7
 import XCTest
 
-//FIXME: remove when C7 pr merged
-extension Data {
-    public init(start: UnsafePointer<Byte>, count: Int) {
-        let buffer = UnsafeBufferPointer(start: start, count: count);
-        self.bytes = Array(buffer)
-    }
-}
-
 class ExampleTests: XCTestCase {
     let example: MessagePackValue = ["compact": true, "schema": 0]
 
@@ -49,20 +41,13 @@ class ExampleTests: XCTestCase {
     }
     
 
-    func testUnpackNSData() {
-        let data = correctA.withUnsafeBufferPointer { buffer in
-            return Data(start: buffer.baseAddress!, count: buffer.count)
-        }
-
-        let unpacked = try? unpack(data)
+    func testUnpackData() {
+        let unpacked = try? unpack(correctA)
         XCTAssertEqual(unpacked, example)
     }
 
-    func testUnpackInsufficientNSData() {
-        let bytes: Data = [0x82, 0xa7, 0x63, 0x6f, 0x6d, 0x70, 0x61, 0x63, 0x74, 0xc3, 0xa6, 0x73, 0x63, 0x68, 0x65, 0x6d]
-        let data = bytes.withUnsafeBufferPointer { buffer in
-            return Data(start: buffer.baseAddress!, count: buffer.count)
-        }
+    func testUnpackInsufficientData2() {
+        let data: Data = [0x82, 0xa7, 0x63, 0x6f, 0x6d, 0x70, 0x61, 0x63, 0x74, 0xc3, 0xa6, 0x73, 0x63, 0x68, 0x65, 0x6d]
 
         do {
             try unpack(data)
