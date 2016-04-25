424db29c2dc6dc50ad46bc033c3b93ce0b054b30
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-25 01:32:33 -0300

    add travis

---
+++ Tests/MessagePack/MessagePackTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import MessagePack
+
+class MessagePackTests: XCTestCase {
+    func testReality() {
+        XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
+    }
+}
+
+extension MessagePackTests {
+    static var allTests : [(String, MessagePackTests -> () throws -> Void)] {
+        return [
+           ("testReality", testReality),
+        ]
+    }
+}
